13f6cd07b344b0145c1453769fd728cbc5a5d356
GabWT333 <gab333github@gmail.com>
2025-09-03 13:27:15 +0200

    Update gp-chiuso.js

--- plugins/gp-chiuso.js
+++ plugins/gp-chiuso.js
@@ -1,16 +1,5 @@
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    // Get bot name from database or use default
-    let nomeDelBot = global.db.data.nomedelbot || `𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲`
-  
-    let setting = {
-      '': 'announcement'
-    }[args[0] || '']
-    
-    if (setting === undefined) return
-    
-    await conn.groupSettingUpdate(m.chat, setting)
-    
-    // Send message with newsletter forwarding
+let handler = async (m, { conn }) => {
+    await conn.groupSettingUpdate(m.chat, 'announcement')
     await conn.sendMessage(m.chat, {
       text: '𝐂𝐡𝐚𝐭 𝐩𝐞𝐫 𝐬𝐨𝐥𝐢 𝐚𝐝𝐦𝐢𝐧',
       contextInfo: {
@@ -19,16 +8,16 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
         forwardedNewsletterMessageInfo: {
           newsletterJid: '120363259442839354@newsletter',
           serverMessageId: '',
-          newsletterName: `${nomeDelBot}`
+          newsletterName: global.db.data.nomedelbot || `𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲`
         }
       }
     }, { quoted: m })
-  }
-  
-  handler.help = ['group open / close', 'gruppo aperto / chiuso']
-  handler.tags = ['group']
-  handler.command = /^(chiuso)$/i
-  handler.admin = true
-  handler.botAdmin = true
-  
-  export default handler
+}
+
+handler.help = ['group open / close', 'gruppo aperto / chiuso']
+handler.tags = ['group']
+handler.command = /^(chiuso)$/i
+handler.admin = true
+handler.botAdmin = true
+
+export default handler
