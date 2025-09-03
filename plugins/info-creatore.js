2aa6ee68fed86be82466af1965ed4bfab69d517a
laboratiork25 <githubunityy@gmail.com>
2025-09-03 11:04:02 +0200

    Update info-creatore.js

--- plugins/info-creatore.js
+++ plugins/info-creatore.js
@@ -2,50 +2,57 @@ import pkg from 'baileys'
 const { generateWAMessageFromContent } = pkg
 
 let handler = async (m, { conn }) => {
-  // vCard unico contatto
+  // vCard primo contatto
   let vcard1 = `BEGIN:VCARD
 VERSION:3.0
-FN: vale
-ORG: valep
-TEL;type=CELL;type=VOICE;waid=393518419909:+39 351 553 3859
+FN: ˙ . ᵗˢᵏ ꒷ . 𝐧𝐞𝐱𝐮𝐬 𖦹˙🪽
+ORG: Davide¹
+TEL;type=CELL;type=VOICE;waid=393518419909:+39 351 841 9909
 END:VCARD`
 
-  // primo invio -> contatto
+  // vCard secondo contatto
+  let vcard2 = `BEGIN:VCARD
+VERSION:3.0
+FN: ˙ . ᵗˢᵏ ꒷ . Luca 𖦹˙🪽
+ORG: Davide²
+TEL;type=CELL;type=VOICE;waid=393793399399:+39 379 339 9399
+END:VCARD`
+
+  // primo invio -> entrambi i contatti insieme
   await conn.sendMessage(m.chat, {
     contacts: {
-      displayName: "Owner",
+      displayName: "Owners",
       contacts: [
-        { vcard: vcard1 }
+        { vcard: vcard1 },
+        { vcard: vcard2 }
       ]
     }
   }, { quoted: m })
 
-  // secondo invio -> messaggio CTA URL con un bottone
+  // secondo invio -> messaggio CTA URL con più bottoni
   let msg = generateWAMessageFromContent(m.chat, {
     viewOnceMessage: {
       message: {
-        messageContextInfo: {
-          deviceListMetadata: {},
-          deviceListMetadataVersion: 2
-        },
         interactiveMessage: {
-          header: { 
-            title: "I miei social" 
-          },
-          body: { 
-            text: "Puoi contattarmi anche qua: 👇" 
-          },
-          footer: { 
-            text: "MyBot" 
-          },
+          header: { title: "I miei social" },
+          body: { text: "Puoi cobtattarmi anche qua: 👇" },
+          footer: { text: nomebot },
           nativeFlowMessage: {
             buttons: [
               {
                 name: "cta_url",
                 buttonParamsJson: JSON.stringify({
+                  display_text: "『 💻 』 GitHub",
+                  url: "https://github.com/Davjde333",
+                  merchant_url: "https://github.com/Davjde333"
+                })
+              },
+              {
+                name: "cta_url",
+                buttonParamsJson: JSON.stringify({
                   display_text: "『 📸 』 Instagram",
-                  url: "https://instagram.com/yyktv.vale",
-                  merchant_url: "https://instagram.com/yyktv.vale"
+                  url: "https://instagram.com/dxvjde",
+                  merchant_url: "https://instagram.com/dxvjde"
                 })
               }
             ]
@@ -57,8 +64,7 @@ END:VCARD`
 
   await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
 }
-
 handler.help = ['owner']
 handler.tags = ['main']
-handler.command = ['creatore'] 
-export default handler
+handler.command = ['padroni','owner'] 
+export default handlerp
